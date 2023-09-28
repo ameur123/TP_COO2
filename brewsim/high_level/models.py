009a54d19a2986586d8a811e0866ea6b48ca55cb
bouchnak <bouchnakameur.fsm52191@gmail.com>
2023-09-28 14:33:54 +0200

    add high_level models & admin

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -2,40 +2,36 @@ from django.db import models
 
 class Departement(models.Model):
     numero = models.CharField(max_length=10)
-    prix_par_metre_carre = models.CharField(max_digits=10, decimal_places=2)
-    
+    prix_par_metre_carre = models.DecimalField(max_digits=10, decimal_places=2)
+
 class Recette(models.Model):
     nom = models.CharField(max_length=100)
-    actions = models.Foreignkey(Action)
-    on_delete=models.PORTECT
-
+    actions = models.ManyToManyField('Action')
+    
 class Action(models.Model):
-    machine = models.Foreignkey(Machine, on_delete=models.PORTECT)
+    machine = models.ForeignKey('Machine', on_delete=models.PROTECT)
     commande = models.CharField(max_length=100)
     duree = models.IntegerField()
     action = models.CharField(max_length=100)
-    ingerdient = models.ManyToManyFild(Ingredient)
+    ingredients = models.ManyToManyField('Ingredient')
 
 class Ingredient(models.Model):
     nom = models.CharField(max_length=100)
-    
-    
+
 class Usine(models.Model):
-    departement = models.Foreignkey(Departement, on_delete=models.PORTECT)
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     taille = models.DecimalField(max_digits=10, decimal_places=2)
-    machines = models.Foreignkey(Machine, on_delete=models.PORTECT)
-    stock = models.ForeignKey(QuantiteIngredient, on_delete=models.PORTECT)
-    recette_biere = models.ForeignKey(Recette, on_delete=models.PORTECT)
+    machines = models.ManyToManyField('Machine')
+    stock = models.ForeignKey('QuantiteIngredient', on_delete=models.PROTECT)
+    recette_biere = models.ForeignKey(Recette, on_delete=models.PROTECT)
 
 class Machine(models.Model):
-    nom= models.CharField(max_length=100)
+    nom = models.CharField(max_length=100)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
 
-
-
 class Prix(models.Model):
-    ingredients = models.Foreignkey(Ingredient, on_delete=models.PORTECT))
-    departement = models.ForeignKey(Departement, on_delete=models.PORTECT)
+    ingredients = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
 
 class QuantiteIngredient(models.Model):
