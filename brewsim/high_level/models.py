3a989ce02df7c9d5c1f54ba91293f7ba79cbbe12
bouchnak <bouchnakameur.fsm52191@gmail.com>
2023-09-28 15:24:54 +0200

    add high_level models & admin

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -4,10 +4,16 @@ class Departement(models.Model):
     numero = models.CharField(max_length=10)
     prix_par_metre_carre = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"Departement {self.numero}"
+
 class Recette(models.Model):
     nom = models.CharField(max_length=100)
     actions = models.ManyToManyField('Action')
     
+    def __str__(self):
+        return self.nom
+
 class Action(models.Model):
     machine = models.ForeignKey('Machine', on_delete=models.PROTECT)
     commande = models.CharField(max_length=100)
@@ -15,8 +21,14 @@ class Action(models.Model):
     action = models.CharField(max_length=100)
     ingredients = models.ManyToManyField('Ingredient')
 
+    def __str__(self):
+        return f"Action {self.action}"
+
 class Ingredient(models.Model):
     nom = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nom
 
 class Usine(models.Model):
     departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
@@ -25,16 +37,28 @@ class Usine(models.Model):
     stock = models.ForeignKey('QuantiteIngredient', on_delete=models.PROTECT)
     recette_biere = models.ForeignKey(Recette, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"Usine {self.id}"
+
 class Machine(models.Model):
     nom = models.CharField(max_length=100)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nom
 
 class Prix(models.Model):
     ingredients = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
     prix = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return f"Prix pour {self.ingredients} dans {self.departement}"
+
 class QuantiteIngredient(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantite = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantite} de {self.ingredient}"
 
