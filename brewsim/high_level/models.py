bc5824f51c2b1249494e92550ad4e5ad7c1ab3df
bouchnak <bouchnakameur.fsm52191@gmail.com>
2023-10-01 16:01:58 +0200

    setup tooling

--- brewsim/high_level/models.py
+++ brewsim/high_level/models.py
@@ -1,79 +1,79 @@
 from django.db import models
 
+# Create your models here.
 class Departement(models.Model):
-    numero = models.CharField(max_length=10)
-    prix_par_metre_carre = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"Departement {self.numero}"
-    
-    def cost(self):   
-        nombre_de_departements = Departement.objects.count()
-        
-
-class Recette(models.Model):
-    nom = models.CharField(max_length=100)
-    actions = models.ManyToManyField('Action')
-    
-    def __str__(self):
-        return self.nom
-
-class Action(models.Model):
-    machine = models.ForeignKey('Machine', on_delete=models.PROTECT)
-    commande = models.CharField(max_length=100)
-    duree = models.IntegerField()
-    action = models.CharField(max_length=100)
-    ingredients = models.ManyToManyField('Ingredient')
-
-    def __str__(self):
-        return f"Action {self.action}"
-
-class Ingredient(models.Model):
-    nom = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nom
-
-class Usine(models.Model):
-    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
-    taille = models.DecimalField(max_digits=10, decimal_places=2)
-    machines = models.ManyToManyField('Machine')
-    stock = models.ForeignKey('QuantiteIngredient', on_delete=models.PROTECT)
-    recette_biere = models.ForeignKey(Recette, on_delete=models.PROTECT)
-    
-
-    def __str__(self):
-        return f"Usine {self.id}"
-        
-    def calculate_charge(self):
-        # Calcul du prix de charge de l'usine
-        prix_machines = sum(machine.get_prix_machine() for machine in self.machines.all())
-        prix_stock = self.stock.get_prix_ingredient()
-        return prix_machines + prix_stock
+        numero = models.IntegerField()
+        prixparMcarre = models.DecimalField(max_digits=10, decimal_places=2)
+        def __str__(self):
+                return f"{self.numero} {self.prixparMcarre}"
 
 class Machine(models.Model):
-    nom = models.CharField(max_length=100)
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
+        nom = models.CharField(max_length=100)
+        prix = models.IntegerField()
+        def __str__(self):
+                return f"{self.nom} {self.prix}"
+        def __cost__(self):
+                return self.prix
 
-    def __str__(self):
-        return self.nom
-
-class Prix(models.Model):
-    ingredients = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
-    departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
-    prix = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"Prix pour {self.ingredients} dans {self.departement}"
+class Ingredient(models.Model):
+        nom = models.CharField(max_length=100)
+        def __str__(self):
+                return f"{self.nom}"
 
 class QuantiteIngredient(models.Model):
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantite = models.DecimalField(max_digits=10, decimal_places=2)
+        ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+        quantite = models.IntegerField()
+        def __str__(self):
+                return f"{self.ingredient} {self.quantite}"
+        def __cost__(self, departement):
+                return self.ingredient.prix_set.get(departement__numero=departement).prix * self.quantite
 
-    def __str__(self):
-        return f"{self.quantite} de {self.ingredient}"
-        
-    def __str__(self):
-        prix_total = self.ingredient.prix * self.quantite
-        return f"Quantité de {self.ingredient} : {self.quantite} | Prix total : {prix_total} €"
+class Action(models.Model):
+        machine = models.ForeignKey(Machine, on_delete=models.PROTECT)
+        commande = models.IntegerField()
+        duree = models.IntegerField()
+        ingredients = models.ManyToManyField(QuantiteIngredient)
+        action = models.CharField(max_length=100)
+        def __str__(self):
+                return f"{self.machine} {self.commande} {self.duree} {self.ingredients} {self.action}"
 
+class Recette(models.Model):
+        nom = models.CharField(max_length=100)
+        action = models.ForeignKey(Action, on_delete=models.PROTECT)
+        def __str__(self):
+                return f"{self.nom} {self.action}"
+
+class Usine(models.Model):
+        departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
+        taille = models.IntegerField()
+        machines = models.ManyToManyField(Machine)
+        recettes = models.ManyToManyField(Recette)
+        stocks = models.ManyToManyField(QuantiteIngredient)
+        def  costMachines(self):
+            total=0
+            for m in self.machines.all():
+                total= total + m.prix
+            return total
+        def __str__(self):
+                return f"{self.departement} {self.taille} {self.machines} {self.recettes} {self.stocks}"
+        def __cost__(self):
+                return (self.taille * self.departement.prixparMcarre ) + (self.costMachines())
+
+
+class Prix(models.Model):
+        ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+        departement = models.ForeignKey(Departement, on_delete=models.PROTECT)
+        prix = models.IntegerField()
+        def __str__(self):
+                return f"{self.ingredient} {self.departement} {self.prix}"
+
+#Création des objets pour chaque classe
+
+departement=Departement()
+machine=Machine()
+ingredient=Ingredient()
+quantiteingredient=QuantiteIngredient()
+action=Action()
+recette=Recette()
+usine=Usine()
+prix=Prix()
